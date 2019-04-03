86f3f255695d8e1533c0d85943140782bd49d29f
djdjdjdsjsmsmsn <48976966+djdjdjdsjsmsmsn@users.noreply.github.com>
2019-04-03 15:21:08 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main() {
+
+    float a,b,c,d,e,f;
+    a=2.5;
+    b=2;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    printf("%.2f\n",c);
+    printf("%.2f\n",d);
+    printf("%.2f\n",e);
+    printf("%.2f\n",f);
+    return 0;
+}
